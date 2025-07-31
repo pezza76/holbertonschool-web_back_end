46d3bb24c7e55bc61710acf6bd61a59114ee6e94
Andrew Perry <pezza76@hotmail.com>
2025-07-31 15:10:43 +1000

    task6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Module 6"""
-from typing import List
+from typing import List, Union
 
-
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     return sum(mxd_lst)
