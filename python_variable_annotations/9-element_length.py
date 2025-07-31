7a38c4601f1228707100b1356c091057bd199f55
Andrew Perry <pezza76@hotmail.com>
2025-07-31 17:58:36 +1000

    task9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """Module 9"""
 from typing import List, Iterable, Sequence, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
    """return a list of tuples"""
    return [(i, len(i)) for i in lst]
