6de4267422e8b3ee5da3f2815116cf1737326b52
Andrew Perry <pezza76@hotmail.com>
2025-08-13 15:39:09 +1000

    task0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 """pagination"""
 
+
 def index_range(page, page_size):
     """pagination"""
     start = (page - 1) * page_size
     end = page *  page_size
     return start, end
-
-
-
-
-
