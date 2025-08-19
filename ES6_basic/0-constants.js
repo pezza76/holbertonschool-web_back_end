2767c34e2a53edef5082277c1acc7944fb8e8b4c
Andrew Perry <pezza76@hotmail.com>
2025-08-19 17:29:42 +1000

    task0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,9 +1,9 @@
-function taskFirst() {
+export function taskFirst() {
     const task = 'I prefer const when I can';
     return task
 }
 
-function taskNext() {
+export function taskNext() {
     let task = 'But sometimes let is okay';
     return task
 }
