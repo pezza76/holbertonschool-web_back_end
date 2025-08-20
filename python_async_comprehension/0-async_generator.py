aab7daab7a5f6c6a02e9489d19d4a4389a620116
Andrew Perry <pezza76@hotmail.com>
2025-08-20 21:52:37 +1000

    task0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,7 +10,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+def async_generator() -> AsyncGenerator[float, None]:
     """Function yields certain value between 0 and 10
 
     Yields:
