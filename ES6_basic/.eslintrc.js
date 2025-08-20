9f442da040e2fe6ef91333739dbef786a3b08f9c
Andrew Perry <pezza76@hotmail.com>
2025-08-20 22:46:15 +1000

    task5

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   env: {
-    browser: false,
+    browser: true,
     es6: true,
-    jest: true,
   },
   extends: [
     'airbnb-base',
-    'plugin:jest/all',
   ],
   globals: {
     Atomics: 'readonly',
@@ -16,20 +14,6 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['jest'],
   rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
   },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
 };
