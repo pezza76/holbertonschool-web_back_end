e2bd3d5c9d978f9cddd7544e14488128bb574af8
Andrew Perry <pezza76@hotmail.com>
2025-08-21 13:09:42 +1000

    task10

--- ES6_basic/test.js
+++ ES6_basic/test.js
@@ -1,13 +1,7 @@
-const user = {
-  name: 'Alex',
-  age: 30,
-  city: 'Melbourne'
-};
-
-for (const key in user) {
-  console.log(key)
+const arr = {
+  age: 18
 }
 
-for (const key of user) {
-  console.log(key)
+for (const v of arr) {
+  console.log(v)
 }
