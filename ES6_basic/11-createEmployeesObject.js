5d3c01560d12e0f8a392817de9d105fba816ee34
Andrew Perry <pezza76@hotmail.com>
2025-08-21 13:27:55 +1000

    task11

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,9 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
   return {
-    departmentName: departmentName,
-    employees: employees
+    [departmentName]: employees
   };
 }
-
-
-console.log('hr', 4)
