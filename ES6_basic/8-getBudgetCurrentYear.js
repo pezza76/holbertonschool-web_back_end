45960987bfff7277ce7055457c478da7ae5f11bf
Andrew Perry <pezza76@hotmail.com>
2025-08-21 10:11:19 +1000

    task8

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+const y = getCurrentYear();
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+
+  budget[`income-${y()}`] = income;
+  budget[`gdp-${y()}`] = gdp;
+  budget[`capita-${y()}`] = capita;
+
+  return budget;
+}
