7c7069d1b6f40e09ec4597d06af0bf6e8c22eb05
Andrew Perry <pezza76@hotmail.com>
2025-08-28 23:11:02 +1000

    task3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+export default class Currency {
+    constructor(code, name) {
+        this._code = code,
+        this._name = name
+    }
+
+get code() {
+    return this._code;
+}
+
+set code(codename) {
+    this._code = codename;
+}
+
+get name() {
+    return this._name;
+}
+
+set name(setname) {
+    this._name = setname;
+}
+
+displayFullCurrency() {
+    return `${this._name}${(this._code)}`;
+}
+
+};
