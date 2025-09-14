1e3b08e34c8c71d809d656835f68f22d7c479792
Andrew Perry <pezza76@hotmail.com>
2025-09-14 21:25:29 +1000

    task5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -18,18 +18,3 @@ export default class Building {
             throw new Error("Class extending Building must override evacuationWarningMessage");
     }
 }
-
-
-
-
-
-
-
-mplement a class named Building:
-
-Constructor attributes:
-sqft (Number)
-Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
-Implement a getter for each attribute.
-Consider this class as an abstract class. And make sure that any class that extends from it should implement a method named evacuationWarningMessage.
-If a class that extends from it does not have a evacuationWarningMessage method, throw an error with the message Class extending Building must override evacuationWarningMessage
