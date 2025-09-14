9b7077b5cf2999fffa4ef10ce6cdadbc69c31a6a
Andrew Perry <pezza76@hotmail.com>
2025-09-14 14:29:27 +1000

    task5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,21 @@
+/* eslint-disable */
 export default class Building {
     constructor(sqft) {
-        if (this.constructor === Building) {
-            throw new Error('this is an abstract class');
-        }
-
-        if (typeof this.evacuationWarningMessage !== 'function') {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
         this._sqft = sqft;
+        this.evacuationWarningMessage();
     }
 
     get sqft() {
         return this._sqft;
+    }
+
+    set sqft(sqft) {
+        this._sqft = sqft;
+    }
+    
+    evacuationWarningMessage() {
+        if (this.constructor != Building)
+            throw new Error("Class extending Building must override evacuationWarningMessage");
     }
 }
 
